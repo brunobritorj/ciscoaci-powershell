00969e2d64919c557b9cfbe5dc63e0077ca3e7b7
Bruno Brito <39683855+brunobritorj@users.noreply.github.com>
2022-04-26 11:26:01 +0100

    Create CiscoTools.ps1

---
+++ CiscoTools.ps1
@@ -0,0 +1,14 @@
+function CiscoTools-ConvertHexToIP ([parameter(mandatory=$true)][string]$Hex) {
+
+    if ($Hex.ToString() -match '0x') { [string]$Hex = $Hex -replace '0x'}
+
+    $hexPairs = @()
+    $hexPairs += "$($Hex[-8])$($Hex[-7])"
+    $hexPairs += "$($Hex[-6])$($Hex[-5])"
+    $hexPairs += "$($Hex[-4])$($Hex[-3])"
+    $hexPairs += "$($Hex[-2])$($Hex[-1])"
+
+    $ipAddress = ($hexPairs | ForEach { [convert]::ToInt32($_,16) }) -join '.'
+
+    return $ipAddress
+}
