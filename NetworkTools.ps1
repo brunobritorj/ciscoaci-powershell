e5d6d035f53587ab7065486e537d919d2c49ecb2
Bruno Brito <39683855+brunobritorj@users.noreply.github.com>
2022-04-28 16:52:56 +0100

    Update and rename CiscoTools.ps1 to NetworkTools.ps1

--- NetworkTools.ps1
+++ NetworkTools.ps1
@@ -1,4 +1,4 @@
-function CiscoTools-ConvertHexToIP ([parameter(mandatory=$true)][string]$Hex) {
+function NetworkTools-ConvertHexToIP ([parameter(mandatory=$true)][string]$Hex) {
 
     if ($Hex.ToString() -match '0x') { [string]$Hex = $Hex -replace '0x'}
 
@@ -8,7 +8,7 @@ function CiscoTools-ConvertHexToIP ([parameter(mandatory=$true)][string]$Hex) {
     $hexPairs += "$($Hex[-4])$($Hex[-3])"
     $hexPairs += "$($Hex[-2])$($Hex[-1])"
 
-    $ipAddress = ($hexPairs | ForEach { [convert]::ToInt32($_,16) }) -join '.'
+    [IPAddress]$ipAddress = ($hexPairs | ForEach { [convert]::ToInt32($_,16) }) -join '.'
 
-    return $ipAddress
+    return $ipAddress.IPAddressToString
 }
