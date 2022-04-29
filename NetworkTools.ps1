0df67b6edd5f45343321fafff576035ddc828ba4
Bruno Brito <39683855+brunobritorj@users.noreply.github.com>
2022-04-29 18:51:30 +0100

    Update NetworkTools.ps1

--- NetworkTools.ps1
+++ NetworkTools.ps1
@@ -3,7 +3,7 @@ function NetworkTools-ConvertHexToIP ([parameter(mandatory=$true)][string]$Hex) 
     if ($Hex.ToString() -match '0x') { [string]$Hex = $Hex -replace '0x'}
     $hexPairs = @()
     -4..-1 | ForEach-Object { if ("$($Hex[2*$_])$($Hex[(2*$_)+1])") {$hexPairs += "$($Hex[2*$_])$($Hex[(2*$_)+1])"} }
-    [IPAddress]$ipAddress = ($hexPairs | ForEach { [convert]::ToInt32($_,16) }) -join '.'
+    [IPAddress]$ipAddress = ($hexPairs | ForEach-Object { [convert]::ToInt32($_,16) }) -join '.'
 
     return $ipAddress.IPAddressToString
 }
